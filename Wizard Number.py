a2ba4981e2e726ec5630e192581496b368116f5b
oud <oudmadadi@gmail.com>
2025-08-09 14:56:04 +0330

    second commit

--- Wizard Number.py
+++ Wizard Number.py
@@ -1,11 +1,13 @@
 n = int(input("Enter the number:"))
 
-if n % 3 == 0 and n % 5 == 0:
+if n % 3 == 0 and n % 5 == 0 and n % 2 == 0:
     print("Legend")
-elif n % 3 == 0:
+elif n % 2 == 0:
     print("Wizard")
-elif n % 5 == 0:
+elif n % 3 == 0:
     print("Devil")
+elif n % 5 == 0:
+    print ("Code Monkey!")
 else:
     print("Awww!")
 
